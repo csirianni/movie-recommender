06691d06dad401046512f68ceb11ff788295c1b7
Cedric Sirianni <cds1@sirianni.org>
2022-12-21 17:17:32 -0500

    add function for fetching recommendations

--- frontend/src/controller/Recommendation.ts
+++ frontend/src/controller/Recommendation.ts
@@ -1,1 +1,11 @@
-export { }
+export async function getRecommendations(movieIDs: number[]) {
+    const response = await fetch('http://127.0.0.1:5000/recommendations/', {
+        method: 'POST',
+        headers: {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({ "example1": movieIDs[0], "example2": movieIDs[1], "example3": movieIDs[2] })
+    })
+    return await response.json()
+}
